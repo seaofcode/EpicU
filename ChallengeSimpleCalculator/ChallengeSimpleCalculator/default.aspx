879b91852b846ebb57a9cfc68ff3d2779a8de436
Matthew J Pearson <seaofcode@gmail.com>
2016-02-17 16:16:09 -0600

    Publish Simple Calculator

---
+++ ChallengeSimpleCalculator/ChallengeSimpleCalculator/default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ChallengeSimpleCalculator._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Simple Calculator<br />
+        <br />
+        First Value
+        <asp:TextBox ID="firstTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Second Value
+        <asp:TextBox ID="secondTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="addButton" runat="server" OnClick="addButton_Click" Text="+" />
+&nbsp;&nbsp;
+        <asp:Button ID="subtractButton" runat="server" Text="-" OnClick="subtractButton_Click" />
+&nbsp;&nbsp;
+        <asp:Button ID="multiplyButton" runat="server" Text="*" OnClick="multiplyButton_Click" />
+&nbsp;&nbsp;
+        <asp:Button ID="divideButton" runat="server" Text="/" OnClick="divideButton_Click" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeSimpleCalculator/ChallengeSimpleCalculator/default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            int result = int.Parse(firstTextBox.Text) + int.Parse(secondTextBox.Text);
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void subtractButton_Click(object sender, EventArgs e)
+        {
+            int result = int.Parse(firstTextBox.Text) - int.Parse(secondTextBox.Text);
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void multiplyButton_Click(object sender, EventArgs e)
+        {
+            int result = int.Parse(firstTextBox.Text) * int.Parse(secondTextBox.Text);
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void divideButton_Click(object sender, EventArgs e)
+        {
+            int result = int.Parse(firstTextBox.Text) / int.Parse(secondTextBox.Text);
+            resultLabel.Text = result.ToString();
+        }
+    }
+}
