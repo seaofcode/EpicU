ad0a9bbcfd2f374201b954937c91d0d329b6872e
Matthew J Pearson <seaofcode@gmail.com>
2016-02-19 13:32:52 -0600

    Solution Complete

---
+++ ChallengeConditionalRadioButton/ChallengeConditionalRadioButton/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeConditionalRadioButton.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="auto-style1">
+    
+        Your note taking preferences
+        <br />
+        <br />
+        <asp:RadioButton ID="pencilRadioButton" runat="server" GroupName="notes" Text="Pencil" />
+        <br />
+        <asp:RadioButton ID="penRadioButton" runat="server" GroupName="notes" Text="Pen" />
+        <br />
+        <asp:RadioButton ID="phoneRadioButton" runat="server" GroupName="notes" Text="Phone" />
+        <br />
+        <asp:RadioButton ID="tabletRadioButton" runat="server" GroupName="notes" Text="Tablet" />
+        <br />
+        <br />
+        <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="OK" />
+        <br />
+        <br />
+        <asp:Image ID="Image" runat="server" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeConditionalRadioButton/ChallengeConditionalRadioButton/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeConditionalRadioButton
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = "";
+
+            if (pencilRadioButton.Checked)
+            {
+                resultLabel.Text = "You selected pencil";
+                Image.ImageUrl = "~/pencil.png";
+            }
+            else if (penRadioButton.Checked)
+            {
+                resultLabel.Text = "You selected pen";
+                Image.ImageUrl = "~/pen.png";
+            }
+            else if (phoneRadioButton.Checked)
+            {
+                resultLabel.Text = "You selected phone";
+                Image.ImageUrl = "~/phone.png";
+            }
+            else if (tabletRadioButton.Checked)
+            {
+                resultLabel.Text = "You selected tablet";
+                Image.ImageUrl = "~/tablet.png";
+            }
+            else
+            {
+
+                resultLabel.Text = "Please select an option";
+            }
+        }
+    }
+}
