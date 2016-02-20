9546978b51b714ad60c8b294e55d1ff2f585c74a
Matthew J Pearson <seaofcode@gmail.com>
2016-02-20 12:37:22 -0600

    first commit

---
+++ CS-ASP_019/CS-ASP_019/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS_ASP_019.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Loan Application Form<br />
+        <br />
+        Name :
+        <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Phone Number:
+        <asp:TextBox ID="phoneTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Social Security Number:
+        <asp:TextBox ID="ssnTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Loan Origination Date:
+        <br />
+        <asp:Calendar ID="loanDateCalendar" runat="server"></asp:Calendar>
+        <br />
+        Salary: <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ CS-ASP_019/CS-ASP_019/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_019
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //string result = String.Format("Thank you, {0}, for your business", nameTextBox.Text);
+            int ssn = int.Parse(ssnTextBox.Text);
+            //string result = String.Format("Thank you, {0}, Your social security Number is {1:000-00-0000}", nameTextBox.Text, ssn);
+
+            int phone = int.Parse(phoneTextBox.Text);
+            //string result = String.Format("Thank you, {0}, Your social security Number is {1:000-00-0000}", nameTextBox.Text, ssn);
+            
+            string result = String.Format("Thank you, {0}, for your business. " 
+                + "<br />Your Social Security Number is: {1:000-00-0000}"
+                + "<br />Phone: {2: (000) 000-0000}"
+                + "<br />Loan Date: {3:ddd -- d, M, yy}",
+                nameTextBox.Text,
+                ssn,
+                phone,
+                loanDateCalendar.SelectedDate);
+
+            resultLabel.Text = result;
+        }
+    }
+}
