9108bff7fd5bb3aaaaf7efb4865e915307a014b6
Matthew J Pearson <seaofcode@gmail.com>
2016-02-23 22:00:20 -0600

    second commit

---
+++ ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/Deafult.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Deafult.aspx.cs" Inherits="ChallengeEpicSpiesAssignment.Deafult" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin: auto;
+            width: 120px;
+            max-height: 190px;
+            font-size: xx-large;
+        }
+        .auto-style2 {
+            font-size: xx-large;
+        }
+        .auto-style3 {
+            font-size: xx-large;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <img alt="spy_logo" class="auto-style1" src="epic-spies-logo.jpg" /><br class="auto-style2" />
+        <br class="auto-style2" />
+        <span class="auto-style3">Spy New Assignment Form</span><br />
+        </div>
+        <p>
+            Spy Code Name :
+            <asp:TextBox ID="spyNameTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            New Assignment Name :
+            <asp:TextBox ID="assignmentNameTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            End Date of Previous Assignment</p>
+        <p>
+            <asp:Calendar ID="previousCalendar" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            Start Date of New Assignment</p>
+        <p>
+            <asp:Calendar ID="startCalendar" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            End Date of New Assignment</p>
+        <p>
+            <asp:Calendar ID="endCalendar" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            <asp:Button ID="assignButton" runat="server" OnClick="assignButton_Click" Text="Assign Spy" />
+        </p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/Deafult.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssignment
+{
+    public partial class Deafult : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                //Initialize Calendars
+                previousCalendar.SelectedDate = DateTime.Now.Date;
+                startCalendar.SelectedDate = DateTime.Now.Date.AddDays(14);
+                endCalendar.SelectedDate = DateTime.Now.Date.AddDays(21);
+            }
+            Page.MaintainScrollPositionOnPostBack = true;
+        }
+
+        protected void assignButton_Click(object sender, EventArgs e)
+        {
+            // Get Number of days against cost of spy per day
+            TimeSpan assignmentLength = endCalendar.SelectedDate.Subtract(startCalendar.SelectedDate);
+            double totalCost = assignmentLength.TotalDays * 500.00;
+
+            // If current assignment is more than 21 days + $1000
+            if (assignmentLength.TotalDays > 21)
+            {
+                totalCost += 1000.00;              
+            }
+
+            //If past assignment was 14 days ago
+            TimeSpan previousAssignmentLength = startCalendar.SelectedDate.Subtract(previousCalendar.SelectedDate);
+            double daysFromPreviousAssignment = previousAssignmentLength.TotalDays;
+
+            if (daysFromPreviousAssignment >= 14)
+            {
+                totalCost.ToString();
+                resultLabel.Text = "Assignment of " +
+                    spyNameTextBox.Text + " for Assignment " +
+                    assignmentNameTextBox.Text + " is Authorized " +
+                    "<br />Total Budget is : " + totalCost;
+               
+                    
+            }
+            else
+            {
+                resultLabel.Text = "Error: " +
+                "Start Date must be 2 weeks from Previous Assignment Date ";
+                startCalendar.SelectedDate = previousCalendar.SelectedDate.Date.AddDays(14);
+            }
+        }
+    }
+}
