3582964429173c0d56a57b4c1146d938755c657a
Matthew J Pearson <seaofcode@gmail.com>
2016-02-27 10:14:45 -0600

    name array

---
+++ CS-ASP_026/CS-ASP_026/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_026
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string result = "";
+
+            /*
+            for (int i = 0; i < 10; i++)
+            {
+                result += "<br />" + i.ToString();
+            }
+            */
+
+            /*
+            for (int i = 5; i <= 15; i++)
+            {
+                result += "<br />" + i.ToString();
+            }
+            */
+
+            /*
+            for (int i = 15; i > 5; i--)
+            {
+                result += "<br />" + i.ToString();
+            }
+            */
+
+            /*
+            for (int i = 0; i < 15; i+=3)
+            {
+                result += "<br />" + i.ToString();
+            }
+            */
+
+            string[] names = new string[] { "Wolverine", "Cyclops", "Professor X", "Phoenix" };
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                result += "<br />" + names[i];
+            }
+
+            resultLabel.Text = result;
+        }
+    }
+}
