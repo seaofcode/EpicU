aff0598fe3547c139f41c587bf5ea8ca51877e99
Matthew J Pearson <seaofcode@gmail.com>
2016-03-12 07:51:37 -0600

    set up Character class Properties Attack and Defend methods

---
+++ ChallengeHeroMonsterClasses/ChallengeHeroMonsterClasses/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeHeroMonsterClasses
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int DamageMaximum { get; set; }
+        public bool AttackBonus { get; set; }
+
+        public int Attack()
+        {
+            Random random = new Random();
+            int damage = random.Next(this.DamageMaximum);
+            return damage;
+        }
+
+        public void Defend(int damage)
+        {
+            this.Health -= damage;
+        }
+    }
+}
