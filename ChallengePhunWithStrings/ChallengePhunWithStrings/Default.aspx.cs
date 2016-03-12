fd6d96b198c8588319e28d8de24be1ab52452775
Matthew J Pearson <seaofcode@gmail.com>
2016-03-12 02:27:36 -0600

    challenge 4 complete

--- ChallengePhunWithStrings/ChallengePhunWithStrings/Default.aspx.cs
+++ ChallengePhunWithStrings/ChallengePhunWithStrings/Default.aspx.cs
@@ -12,14 +12,49 @@ namespace ChallengePhunWithStrings
         protected void Page_Load(object sender, EventArgs e)
         {
             // Reverse your name
+            /*
             string name = "Matthew Pearson";
 
             for (int i = name.Length - 1; i >= 0; i--)
             {
                 resultLabel.Text += name[i];
             }
+            */
 
+            // Reverse this sequence
+            //string names = "Luke,Leia,Han,Chewy";
+            /*
+            string[] rebelScum = names.Split(',');
+            string result = "";
+            for (int i = rebelScum.Length - 1; i >= 0; i--)
+            {
+                result += rebelScum[i] + ",";
+            }
+            result = result.Remove(result.Length - 1, 1);
+            resultLabel.Text = "<br/> " + result;
+            */
 
+            /*string[] rebelScum = names.Split(',');
+            string result = "";
+            for (int i = 0; i < rebelScum.Length; i++)
+            {
+                int padLeft = (14 - rebelScum[i].Length) / 2;
+                string temp = rebelScum[i].PadLeft(rebelScum[i].Length + padLeft, '*');
+                result += temp.PadRight(14, '*');
+                result += "<br />";
+            }
+            */
+
+            string puzzle = "NOW IS ZHEremove-me ZIME FOR ALL GOOD ME ZO COME ZO ZHE AID OF ZHEIR COUNTRY.";
+            string removeMe = "remove-me";
+            int index = puzzle.IndexOf(removeMe);
+            puzzle = puzzle.Remove(index, removeMe.Length);
+            puzzle = puzzle.ToLower();
+            puzzle = puzzle.Replace('z', 't');
+            puzzle = puzzle.Remove(0, 1);
+            puzzle = puzzle.Insert(0, "N");
+
+            resultLabel.Text = puzzle;
         }
     }
 }
