84b09ff2aae03208f7e7ede50e47af46a75b41fc
Matthew J Pearson <seaofcode@gmail.com>
2016-03-12 01:55:38 -0600

    #1 - Reverse Name

---
+++ ChallengePhunWithStrings/ChallengePhunWithStrings/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePhunWithStrings
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Reverse your name
+            string name = "Matthew Pearson";
+
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                resultLabel.Text += name[i];
+            }
+
+
+        }
+    }
+}
