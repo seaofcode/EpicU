217c854fa8a607f343984a39bb25e4bc20df6906
Matthew J Pearson <seaofcode@gmail.com>
2016-03-12 02:57:10 -0600

    Class created and properties

---
+++ CS-ASP_036/CS-ASP_036/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_036
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+    }
+}
