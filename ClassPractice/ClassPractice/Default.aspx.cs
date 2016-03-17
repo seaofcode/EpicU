2b79b3e638d149a5ec2a0696e4f1bcb8a4f3fd1c
Matthew J Pearson <seaofcode@gmail.com>
2016-03-17 07:55:53 -0500

    Added Car Class and Properties

--- ClassPractice/ClassPractice/Default.aspx.cs
+++ ClassPractice/ClassPractice/Default.aspx.cs
@@ -14,4 +14,12 @@ namespace ClassPractice
 
         }
     }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+    }
 }
