19d7a2ba89519aaf27546b892b77f319026e1931
Matthew J Pearson <seaofcode@gmail.com>
2016-03-17 15:10:57 -0500

    added a class to a new file

---
+++ ClassPractice/ClassPractice/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClassPractice
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public double DetermineMarketValue()
+        {
+            double carValue;
+
+            if (Year > 1990)
+                carValue = 2000.000;
+            else
+                carValue = 1000.000;
+            return carValue;
+        }
+    }
+}
--- ClassPractice/ClassPractice/Default.aspx.cs
+++ ClassPractice/ClassPractice/Default.aspx.cs
@@ -23,23 +23,4 @@ namespace ClassPractice
             resultLabel.Text = String.Format("{0} {1} {2} {3} {4:C}", myNewCar.Make, myNewCar.Model, myNewCar.Year.ToString(), myNewCar.Color, myMarketValue);
         }
     }
-
-    class Car
-    {
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public int Year { get; set; }
-        public string Color { get; set; }
-
-        public double DetermineMarketValue()
-        {
-            double carValue;
-
-            if (Year > 1990)
-                carValue = 2000.000;
-            else
-                carValue = 1000.000;
-            return carValue;
-        }
-    }
 }
