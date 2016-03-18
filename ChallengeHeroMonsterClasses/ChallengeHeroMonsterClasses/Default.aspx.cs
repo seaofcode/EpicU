0b504e467271843fe98983b47f8b5f109391538f
Matthew J Pearson <seaofcode@gmail.com>
2016-03-18 04:29:39 -0500

    class practice, outline with code comments

--- ChallengeHeroMonsterClasses/ChallengeHeroMonsterClasses/Default.aspx.cs
+++ ChallengeHeroMonsterClasses/ChallengeHeroMonsterClasses/Default.aspx.cs
@@ -11,51 +11,17 @@ namespace ChallengeHeroMonsterClasses
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Character hero = new Character();
-            hero.Name = "Maximus";
-            hero.Health = 25;
-            hero.DamageMaximum = 15;
-            hero.AttackBonus = false;
+            // Create two instances of Character
+            // Set their properites
+            
+            // get random vaule from Attack() = damage variable 
+            // equal that value to the parameter passed in to Defend(damage)
+            // Do the sequence again but switching object Attack() and Defend()
 
-            Character monster = new Character();
-            monster.Name = "Balrog";
-            monster.Health = 50;
-            monster.DamageMaximum = 20;
-            monster.AttackBonus = true;
-
-            int damage = hero.Attack();
-            monster.Defend(damage);
-
-            damage = monster.Attack();
-            hero.Defend(damage);
-
-            printResults(hero);
-            printResults(monster);
+            //Print results using helper helper
         }
 
-        private void printResults(Character character)
-        {
-            resultLabel.Text += String.Format("<p>Name: {0} Health: {1} DamageMaximum: {2} AttackBonus: {3}</p>", character.Name, character.Health, character.DamageMaximum, character.AttackBonus);
-        }
-    }
-
-    class Character
-    {
-        public string Name { get; set; }
-        public int Health { get; set; }
-        public int DamageMaximum { get; set; }
-        public bool AttackBonus { get; set; }
-
-        public int Attack()
-        {
-            Random random = new Random();
-            int damage = random.Next(this.DamageMaximum);
-            return damage;
-        }
-
-        public void Defend(int damage)
-        {
-            this.Health -= damage;
-        }
+        // private void printResults(Character character)
+        // Format the instance properties within a paragraph tag
     }
 }
