259c9b809214e1460da40fcc2fe81bbb38a1700f
Matthew J Pearson <seaofcode@gmail.com>
2016-03-18 07:53:44 -0500

    set two object's properties and printed them to the screen

--- ChallengeHeroMonsterClasses/ChallengeHeroMonsterClasses/Default.aspx.cs
+++ ChallengeHeroMonsterClasses/ChallengeHeroMonsterClasses/Default.aspx.cs
@@ -13,13 +13,28 @@ namespace ChallengeHeroMonsterClasses
         {
             // Make two objects of class Character
             Character hero = new Character();
-            // Name, Health, MaximumDamage and AttackBonus properties
+            hero.Name = "Maximus";
+            hero.Health = 100;
+            hero.MaximumDamage = 50;
+            hero.AttackBonus = false;
+
+            Character monster = new Character();
+            monster.Name = "Balrog";
+            monster.Health = 150;
+            monster.MaximumDamage = 75;
+            monster.AttackBonus = true;
 
             // Hero's Combat Round
 
             // Monster's Combat Rounds
 
             // Print Results
+            printResults(hero);
+        }
+
+        private void printResults(Character character)
+        {
+             resultLabel.Text = String.Format("</p>Name: {0} Health: {1} Maximum Damage: {2} Attack Bonus: {3}</p>", character.Name, character.Health, character.MaximumDamage, character.AttackBonus );
         }
     }
 }
