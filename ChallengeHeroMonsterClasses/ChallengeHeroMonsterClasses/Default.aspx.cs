ad17c774f8d60da2756a8e1b7c31d106e5d60b6c
Matthew J Pearson <seaofcode@gmail.com>
2016-03-18 07:31:33 -0500

    class practice start

--- ChallengeHeroMonsterClasses/ChallengeHeroMonsterClasses/Default.aspx.cs
+++ ChallengeHeroMonsterClasses/ChallengeHeroMonsterClasses/Default.aspx.cs
@@ -11,35 +11,7 @@ namespace ChallengeHeroMonsterClasses
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // Create two Objects of Character class
-            Character hero = new Character();
-            hero.Name = "Maximus";
-            hero.Health = 50;
-            hero.MaximumDamage = 15;
-            hero.AttackBonus = false;
-
-            Character monster = new Character();
-            monster.Name = "Balrog";
-            monster.Health = 75;
-            monster.MaximumDamage = 20;
-            monster.AttackBonus = true;
-
-            // Hero Attack Round
-            int damage = hero.Attack();
-            monster.Defend(damage);
-
-            // Monster Attack Round
-            damage = monster.Attack();
-            hero.Defend(damage);
-
-            // Print results to screen
-            printResults(hero);
-            printResults(monster);
-        }
-
-        private void printResults(Character character)
-        {
-            resultLabel.Text += String.Format("<p>Name: {0} - Health: {1} - MaximumDamage: {2} - AttackBonus: {3}</p>", character.Name, character.Health, character.MaximumDamage, character.AttackBonus);
+           
         }
     }
 }
