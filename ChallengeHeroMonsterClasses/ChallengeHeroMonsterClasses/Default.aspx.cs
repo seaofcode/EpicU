83017f5fe3d2496517de33e37025a2675bc34ffb
Matthew J Pearson <seaofcode@gmail.com>
2016-03-18 12:43:57 -0500

    MonsterHero Challenge 2 complete

--- ChallengeHeroMonsterClasses/ChallengeHeroMonsterClasses/Default.aspx.cs
+++ ChallengeHeroMonsterClasses/ChallengeHeroMonsterClasses/Default.aspx.cs
@@ -24,17 +24,40 @@ namespace ChallengeHeroMonsterClasses
             monster.MaximumDamage = 75;
             monster.AttackBonus = true;
 
-            // Hero's Combat Round
+            Dice dice = new Dice();
 
-            // Monster's Combat Rounds
+            //Bonus Attack 
+            if (hero.AttackBonus)
+                monster.Defend(hero.Attack(dice));
+            if (monster.AttackBonus)
+                hero.Defend(monster.Attack(dice));
 
-            // Print Results
-            printResults(hero);
+            //Battle Loop
+            while (hero.Health >= 0 && monster.Health >= 0)
+            {
+                hero.Defend(monster.Attack(dice));
+                monster.Defend(hero.Attack(dice));
+
+                printResults(hero);
+                printResults(monster);
+            }
+
+            displayResults(hero, monster);
+        }
+
+        private void displayResults(Character opponent1, Character opponent2)
+        {
+            if (opponent1.Health <= 0 && opponent2.Health <= 0)
+                resultLabel.Text += String.Format("<p>{0} and {1} both lose</p>", opponent1.Name, opponent2.Name);
+            else if (opponent1.Health <= 0)
+                resultLabel.Text += String.Format("<p>{0} deafeated {1}</p>", opponent2.Name, opponent1.Name);
+            else
+                resultLabel.Text += String.Format("<p>0} defeated {1}</p>", opponent1.Name, opponent2.Name);
         }
 
         private void printResults(Character character)
         {
-             resultLabel.Text = String.Format("</p>Name: {0} Health: {1} Maximum Damage: {2} Attack Bonus: {3}</p>", character.Name, character.Health, character.MaximumDamage, character.AttackBonus );
+             resultLabel.Text += String.Format("</p>Name: {0} - Health: {1} - Maximum Damage: {2} - Attack Bonus: {3} - </p>", character.Name, character.Health, character.MaximumDamage, character.AttackBonus );
         }
     }
 }
---
+++ ChallengeHeroMonsterClasses/ChallengeHeroMonsterClasses/Dice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChallengeHeroMonsterClasses
+{
+    public class Dice
+    {
+        public int Sides { get; set; }
+
+        Random random = new Random();
+        public int Roll()
+        {
+            return random.Next(Sides);
+        }
+    }
+}
