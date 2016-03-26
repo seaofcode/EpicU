585bebe5e4491cdab889e74556d71282788f3dd3
Matthew J Pearson <seaofcode@gmail.com>
2016-03-26 03:04:22 -0500

    added prebuilt project to directory

--- ChallengeStudentCourses/ChallengeStudentCourses/Properties/AssemblyInfo.cs
+++ ChallengeStudentCourses/ChallengeStudentCourses/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ChallengeStudentCourses")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1cc415ce-238c-4753-8b70-9c35b923dfce")]
+[assembly: Guid("adc82439-722b-4c68-aef2-b647005e5643")]
 
 // Version information for an assembly consists of the following four values:
 //
