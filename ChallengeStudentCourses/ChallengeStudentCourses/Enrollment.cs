920d4bff2b621498348c0f1646e76dd5d38c8004
Matthew J Pearson <seaofcode@gmail.com>
2016-03-28 16:51:13 -0500

    finished Enrollment class, added to Student Class

--- ChallengeStudentCourses/ChallengeStudentCourses/Enrollment.cs
+++ ChallengeStudentCourses/ChallengeStudentCourses/Enrollment.cs
@@ -7,6 +7,8 @@ namespace ChallengeStudentCourses
 {
     public class Enrollment
     {
-
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+        public int Grade { get; set; }
     }
 }
