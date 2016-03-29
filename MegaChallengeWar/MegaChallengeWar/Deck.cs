c5c6f0e55e351608316866822c9f26dc53c88967
Matthew J Pearson <seaofcode@gmail.com>
2016-03-29 11:41:44 -0500

    Deck suits and kinds Arrays, foreach loop adding a suit and kind to the _deck collection

--- MegaChallengeWar/MegaChallengeWar/Deck.cs
+++ MegaChallengeWar/MegaChallengeWar/Deck.cs
@@ -13,6 +13,17 @@ namespace MegaChallengeWar
         public Deck()
         {
             _deck = new List<Card>();
+
+            string[] suits = new string[] { "Clubs", "Diamonds", "Hearts", "Spades" };
+            string[] kinds = new string[] {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+
+            foreach (var suit in suits)
+            {
+                foreach (var kind in kinds)
+                {
+                    _deck.Add(new Card() {Suit = suit, Kind = kind });
+                }
+            }
         } 
 
         public void Deal()
