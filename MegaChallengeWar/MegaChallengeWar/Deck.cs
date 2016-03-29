e53aaf5702b9de26d2c3d68fad3c51d975c6c961
Matthew J Pearson <seaofcode@gmail.com>
2016-03-29 11:09:37 -0500

    Deck Class - added new List var, public Deal method and created new instance in constructor

---
+++ MegaChallengeWar/MegaChallengeWar/Deck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MegaChallengeWar
+{
+    public class Deck
+    {
+        private List<Card> _deck;
+
+        //Constructor
+        public Deck()
+        {
+            _deck = new List<Card>();
+        } 
+
+        public void Deal()
+        {
+
+        }
+    }
+}
