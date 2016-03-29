55cfcb57807bd39696da1532adf87c4ba1639ff6
Matthew J Pearson <seaofcode@gmail.com>
2016-03-29 14:15:55 -0500

    added to Deck class - Deal method and dealCard method

--- MegaChallengeWar/MegaChallengeWar/Deck.cs
+++ MegaChallengeWar/MegaChallengeWar/Deck.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace MegaChallengeWar
@@ -8,11 +9,15 @@ namespace MegaChallengeWar
     public class Deck
     {
         private List<Card> _deck;
+        private Random _random;
+        private StringBuilder sb;
 
         //Constructor
         public Deck()
         {
             _deck = new List<Card>();
+            _random = new Random();
+            sb = new StringBuilder();
 
             string[] suits = new string[] { "Clubs", "Diamonds", "Hearts", "Spades" };
             string[] kinds = new string[] {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
@@ -26,9 +31,28 @@ namespace MegaChallengeWar
             }
         } 
 
-        public void Deal()
+        public string Deal(Player player1, Player player2)
         {
+            while (_deck.Count > 0)
+            {
+                dealCard(player1);
+                dealCard(player2);
+            }
+            return sb.ToString();
+        }
 
+        private void dealCard(Player player)
+        {
+            Card card = _deck.ElementAt(_random.Next(_deck.Count));
+            player.Cards.Add(card);
+            _deck.Remove(card);
+
+            sb.Append("</br>");
+            sb.Append(player.Name);
+            sb.Append("is dealt ");
+            sb.Append(card.Kind);
+            sb.Append(" of ");
+            sb.Append(card.Suit);
         }
     }
 }
