466f9fc5ed725e77649adc923f8d01dc59ae8c95
Matthew J Pearson <seaofcode@gmail.com>
2016-04-11 14:42:58 -0500

    added new Battle class, moved getCard( ) and performEvaluation( ) from Game.cs

--- MegaChallengeWar/MegaChallengeWar/Game.cs
+++ MegaChallengeWar/MegaChallengeWar/Game.cs
@@ -42,22 +42,7 @@ namespace MegaChallengeWar
         }
 
 
-        private Card getCard(Player player)
-        {
-            Card card = player.Cards.ElementAt(0);
-            player.Cards.Remove(card);
-            _bounty.Add(card);
-            return card;
-        }
-
-        private void performEvaluation(Player player1, Player player2, Card card1, Card card2)
-        {
-            if (card1.CardValue() > card2.CardValue())
-                player1.Cards.AddRange(_bounty);
-            else
-                player2.Cards.AddRange(_bounty);
-            _bounty.Clear();
-        }
+       
 
         private string determineWinner()
         {
