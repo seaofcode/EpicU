d284abb32e946f92c8b6ec136397e1ededd3da8f
Matthew J Pearson <seaofcode@gmail.com>
2016-05-07 00:36:30 -0500

    Dogs and HomeControllers

---
+++ DogDays/DogDays/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using DogDays.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DogDays.Controllers
+{
+    public class HomeController : Controller
+    {
+        SqlConnection Conn = new SqlConnection("Server=localhost;Database=Example;Trusted_Connection=True;");
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var list = Conn.Query<Dog>("Select DogID, DogName from Table_1");
+            return View(list);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && Conn != null)
+            {
+                Conn.Dispose();
+                Conn = null;
+
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DogDays/DogDays/Models/Dog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DogDays.Models
+{
+    public class Dog
+    {
+        public int DogID { get; set; }
+        public string DogName { get; set; }
+    }
+}
